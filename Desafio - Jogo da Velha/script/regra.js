ad90a955ecfe027c71cf805cb6b38bbb83bcc73f
Kyrsten Júnior <101148038+kyrstenjunior@users.noreply.github.com>
2022-05-20 11:47:01 -0300

    atualização Jogo da Velha

---
+++ Desafio - Jogo da Velha/script/regra.js
@@ -0,0 +1,18 @@
+// Passo 1: iniciar minhas variáveis
+let board = ['', '', '', '', '', '', '', '', ''];
+let playerTime = 0;
+let symbols = ['o', 'x'];
+
+function handleMove(position) {
+
+    if (board[position] == '') {
+
+        board[position] = symbols[playerTime];
+
+            if (playerTime == 0) {
+                playerTime = 1;
+            } else {
+                playerTime = 0;
+            }
+    }
+}
